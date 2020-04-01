093b96ee03637533706682cdb3bd968e0250c5ff
Ngu Duong <nguduong@ansuzdev.com>
2020-04-01 20:42:36 +0700

    update excercise 2

--- exercises/exercise_2/exercise_2.go
+++ exercises/exercise_2/exercise_2.go
@@ -6,10 +6,38 @@ import (
 
 func wordCount(s string) map[string]int {
   // write code here
+  bytes := []byte(s)
+  words := map[string]int{}
+
+  start := 0
+  for index, b := range bytes {
+    if b == ' ' {
+      word := string(bytes[start:index])
+      if len(word) > 0 {
+        if _, ok := words[word]; ok {
+          words[word]++
+        } else {
+          words[word] = 1
+        }
+      }
+
+      start = index + 1
+    }
+  }
+
+  // last word
+  word := string(bytes[start:])
+  if _, ok := words[word]; ok {
+    words[word]++
+  } else {
+    words[word] = 1
+  }
+
+  return words
 }
 
 func main() {
-  str := "lorem ipsum dolor sit amet"
+  str := "lorem ipsum dolor sit amet dolor sit amet"
   result := wordCount(str)
   fmt.Println(result)
 }
