c5a19541167674a74c6c2366477c8290ebdbfb1d
Ngu Duong <nguduong@ansuzdev.com>
2020-04-01 20:21:05 +0700

    add exercise 2

---
+++ exercises/exercise_2.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+)
+
+func wordCount(s string) map[string]int {
+  // write code here
+}
+
+func main() {
+  str := "lorem ipsum dolor sit amet"
+  result := wordCount(str)
+  fmt.Println(result)
+}
