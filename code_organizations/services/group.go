4cdeaecd17bc534f5588c3ab979b372bb825561e
Ngu Duong <nguduong@ansuzdev.com>
2020-04-03 15:34:18 +0700

    add private function

--- code_organizations/services/group.go
+++ code_organizations/services/group.go
@@ -1,8 +1,15 @@
 package services
 
 import (
+  "fmt"
   "golang-samples/code_organizations/models"
 )
+
+// a private function starts with lowercase
+// and will not be exported
+func testGroup() {
+  fmt.Println("Test group function")
+}
 
 // CreateGroup creates a new group with given name
 func CreateGroup(name string) (group *models.Group) {
