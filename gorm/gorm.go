8704b1ca793c843ed2116a9f00f6615adbe40aa7
Ngu Duong <nguduong@ansuzdev.com>
2020-04-13 15:17:58 +0700

    add gorm sample

---
+++ gorm/gorm.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+  "fmt"
+  _ "github.com/go-sql-driver/mysql"
+  "time"
+)
+
+import "github.com/jinzhu/gorm"
+
+type Account struct {
+  ID           int       `gorm:"PRIMARY_KEY;AUTO_INCREMENT"`
+  Username     string    `gorm:"type:VARCHAR(256);NOT NULL"`
+  PasswordHash string    `gorm:"type:VARCHAR(64);NOT NULL"`
+  State        string    `gorm:"type:VARCHAR(64);NOT NULL"`
+  CreatedAt    time.Time `gorm:"NOT NULL"`
+  UpdatedAt    time.Time `gorm:"NOT NULL"`
+  Posts        []Post    `gorm:"foreignkey:AccountId"`
+}
+
+type Post struct {
+  ID        int       `gorm:"PRIMARY_KEY;AUTO_INCREMENT"`
+  AccountId int       `gorm:"NOT NULL"`
+  Content   string    `gorm:"NOT NULL"`
+  CreatedAt time.Time `gorm:"NOT NULL"`
+  UpdatedAt time.Time `gorm:"NOT NULL"`
+}
+
+func connect(connection string) *gorm.DB {
+
+  // Create connection
+  db, err := gorm.Open("mysql", connection)
+  if err != nil {
+    panic(err)
+  }
+
+  err = db.AutoMigrate(&Account{}, &Post{}).Error
+  if err != nil {
+    panic(err)
+  }
+  db.Model(&Post{}).AddForeignKey("account_id", "accounts(id)", "CASCADE", "CASCADE")
+
+  return db
+}
+
+func main() {
+  db := connect("dev:root@tcp(127.0.0.1:3306)/contacts?parseTime=true")
+  defer db.Close()
+  fmt.Println("Connected !")
+
+  // create a new account
+  acc1 := Account{
+    Username:     "user_1",
+    PasswordHash: "password_1",
+    State:        "ENABLED",
+  }
+  err := db.Create(&acc1).Error
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  // Create a post
+  post1 := Post{Content: "A new post!"}
+  err = db.Model(&acc1).Association("Posts").Append(post1).Error
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  // Get an account
+  var readAcc Account
+  err = db.First(&readAcc, 6).Error
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println(readAcc)
+
+  // Update the account
+  readAcc.Username = "user_updated"
+  err = db.Save(&readAcc).Error
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  // Get posts related to account
+  var posts []Post
+  err = db.Model(&readAcc).Related(&posts).Error
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println(posts)
+
+  // Delete an account
+  err = db.Where("id = ?", 1).First(&readAcc).Error
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  db.Delete(&readAcc)
+}
